8b68096bc19216648682928fb441150ca5cbdc1a
Nam Nguyen <hnamnguyencse@gmail.com>
2019-05-25 23:49:19 +0700

    Fix https, hope that this is the last commit

--- src/controllers/email.ts
+++ src/controllers/email.ts
@@ -5,22 +5,22 @@ moment.locale("de");
 let i = 0;
 
 export let registered = async (student: any, activity: any) => {
-    mail.registered(student.fullName, student.email, activity.name, "http://localhost:3000/activity-detail/" + activity._id);
-    console.log("Sending " + ++i + " mail(s) to " + student.fullName + " " + student.email + " " + activity.name + " " + "http://localhost:3000/activity-detail/" + activity._id);
+    mail.registered(student.fullName, student.email, activity.name, "https://sociofy.lyhc.tk/activity-detail/" + activity._id);
+    console.log("Sending " + ++i + " mail(s) to " + student.fullName + " " + student.email + " " + activity.name + " " + "https://sociofy.lyhc.tk/activity-detail/" + activity._id);
 };
 export let refused = async (student: any, activity: any) => {
-    mail.refused(student.fullName, student.email, activity.name, "http://localhost:3000/activity-detail/" + activity._id);
-    console.log("Sending " + ++i + " mail(s) to " + student.fullName + " " + student.email + " " + activity.name + " " + "http://localhost:3000/activity-detail/" + activity._id);
+    mail.refused(student.fullName, student.email, activity.name, "https://sociofy.lyhc.tk/activity-detail/" + activity._id);
+    console.log("Sending " + ++i + " mail(s) to " + student.fullName + " " + student.email + " " + activity.name + " " + "https://sociofy.lyhc.tk/activity-detail/" + activity._id);
 };
 export let reminder = async () => {
-    const activities = await Activity.find({status: true});
+    const activities = await Activity.find({ status: true });
     activities.forEach(activity => {
         const myDate = moment(new Date(activity.dateStart), "MM/DD/YYYY").subtract(1, "days").isSame(moment((new Date()).toLocaleDateString(), "MM/DD/YYYY"));
         if (myDate) {
             const members = activity.members;
             members.forEach(member => {
                 if (member.status == Status.ACCEPT) {
-                    mail.remind(member.info.fullName, member.info.email, activity.name, activity.dateStart, "http://localhost:3000/activity-detail/" + activity._id);
+                    mail.remind(member.info.fullName, member.info.email, activity.name, activity.dateStart, "https://sociofy.lyhc.tk/activity-detail/" + activity._id);
                 }
             });
         }
