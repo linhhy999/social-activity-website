6349076ab999e225ffad75568bd2e39d5a36c2b0
Nam Nguyen <hnamnguyencse@gmail.com>
2019-05-25 23:27:58 +0700

    Comment time, volunteer view width

--- src/controllers/activity.ts
+++ src/controllers/activity.ts
@@ -353,14 +353,11 @@ Date.prototype.toIsoString = function () {
             const norm = Math.floor(Math.abs(num));
             return (norm < 10 ? "0" : "") + norm;
         };
-    return this.getFullYear() +
-        "-" + pad(this.getMonth() + 1) +
-        "-" + pad(this.getDate()) +
-        "T" + pad(this.getHours()) +
-        ":" + pad(this.getMinutes()) +
-        ":" + pad(this.getSeconds()) +
-        dif + pad(tzo / 60) +
-        ":" + pad(tzo % 60);
+    return " " + pad(this.getDate()) +
+        "/" + pad(this.getMonth() + 1) +
+        "/" + this.getFullYear() +
+        " " + pad(this.getHours()) +
+        ":" + pad(this.getMinutes());
 };
 
 export let postComment = async (req: any, res: Response) => {
